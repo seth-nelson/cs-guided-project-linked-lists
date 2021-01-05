e37ec2db0d42b7a6016f67d887ea27e522416bc5
Seth Nelson <seth.nelson@outlook.com>
2021-01-04 16:22:50 -0800

    finsihed demo 2 project file

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -20,4 +20,42 @@ class LinkedListNode():
 
 def reverse(head_of_list):
     # Your code here
-
+    current_node = head_of_list
+    previous_node = None
+    next_node = None
+​
+    while current_node:
+        # set the next_node, preserving access
+        next_node = current_node.next
+​
+        # make current node, point to previous node
+        current_node.next = previous_node
+​
+        # Shift the current and previous pointers to the right
+        previous_node = current_node
+        current_node = next_node
+​
+    return previous_node
+​
+​
+def print_list(head_of_list):
+    # start at head as current_node
+    # iterate through list, printing the values
+    current_node = head_of_list
+    return_str = ''
+    while current_node:
+        return_str += f'({current_node.value}) -> '
+        # move current node forward
+        current_node = current_node.next
+​
+    print(return_str)
+​
+​
+head = LinkedListNode(4)
+head.next = LinkedListNode(3)
+head.next.next = LinkedListNode(2)
+head.next.next.next = LinkedListNode(1)
+​
+print_list(head)
+new_head = reverse(head)
+print_list(new_head)
